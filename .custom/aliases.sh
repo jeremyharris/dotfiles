792df900fa47de21984984177150620dd8b1bab4
Jeremy  Harris <jeremy@someguyjeremy.com>
2013-02-14 09:00:29 -0800

    Added rmws alias

--- .custom/aliases.sh
+++ .custom/aliases.sh
@@ -1,4 +1,6 @@
 # aliases
+
+# remove .DS_Store files recursively
 alias rmds="find . -name '*.DS_Store' -type f -delete"
 alias ls="ls -lah"
 alias lss="ls -1C"
@@ -6,3 +8,5 @@ alias p="cd ~/Projects"
 alias apachelogs="cd /var/log/apache2/"
 alias apacheconf="cd /etc/apache2/"
 alias vim="mvim -v"
+# remove trailing whitespace in all files within current directory, recursively
+alias rmws="find . -type f -exec sed -i 's/[[:space:]]*$//' {} \;"
