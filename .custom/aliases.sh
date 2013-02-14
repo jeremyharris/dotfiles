2c98de45ff5f3af0b6ab0d4d38ca8b5a5e070ed2
Jeremy  Harris <jeremy@someguyjeremy.com>
2013-02-14 09:58:49 -0800

    Fixed rmws breaking git blobs

--- .custom/aliases.sh
+++ .custom/aliases.sh
@@ -9,4 +9,5 @@ alias apachelogs="cd /var/log/apache2/"
 alias apacheconf="cd /etc/apache2/"
 alias vim="mvim -v"
 # remove trailing whitespace in all files within current directory, recursively
-alias rmws="find . -type f -exec sed -i 's/[[:space:]]*$//' {} \;"
+# thanks: http://stackoverflow.com/questions/149057/how-to-remove-trailing-whitespace-of-all-files-recursively?rq=1
+alias rmws="find . -not \( -name .git -prune \) -type f -exec sed -i 's/[[:space:]]*$//' {} \;"
