6a7973f1edae153411ff6b6016266d8de5c094e2
Jeremy Harris <jeremy@42pixels.com>
2013-07-25 07:59:17 -0700

    Removed unused alias

--- .custom/aliases.sh
+++ .custom/aliases.sh
@@ -7,7 +7,6 @@ alias lss="ls -1C"
 alias p="cd ~/Projects"
 alias apachelogs="cd /var/log/apache2/"
 alias apacheconf="cd /etc/apache2/"
-alias vim="mvim -v"
 # remove trailing whitespace in all files within current directory, recursively
 # thanks: http://stackoverflow.com/questions/149057/how-to-remove-trailing-whitespace-of-all-files-recursively?rq=1
 alias rmws="find . -not \( -name .git -prune \) -type f -exec sed -i 's/[[:space:]]*$//' {} \;"
