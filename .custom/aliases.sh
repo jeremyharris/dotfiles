6a0fe8619224e15040d76c6dad3bd5e64aa441d4
Jeremy Harris <jeremy@someguyjeremy.com>
2014-07-23 10:09:24 -0500

    Added ctc aliases

--- .custom/aliases.sh
+++ .custom/aliases.sh
@@ -10,3 +10,7 @@ alias apacheconf="cd /etc/apache2/"
 # remove trailing whitespace in all files within current directory, recursively
 # thanks: http://stackoverflow.com/questions/149057/how-to-remove-trailing-whitespace-of-all-files-recursively?rq=1
 alias rmws="find . -not \( -name .git -prune \) -type f -exec sed -i 's/[[:space:]]*$//' {} \;"
+
+# CTC related aliases
+alias ctc-start-rules='java -jar ~/Projects/CakeDC/ctc/Vendors/Rules/jars/rules.jar -c ~/Projects/CakeDC/ctc/MarsPlatform/lib/CareTeamConnect/Rules/'
+alias ctc-sync='/Users/jeremy/Projects/CakeDC/ctc/MarsPlatform/lib/Cake/Console/cake --app /Users/jeremy/Projects/CakeDC/ctc/MarsPlatform/app sync'
